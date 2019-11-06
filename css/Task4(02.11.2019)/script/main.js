e67c9863cf3c141662643d8c8bc934bc1c88dfee
MariiaRev <maria.revenko2209@gmail.com>
2019-11-06 20:26:19 +0200

    css

---
+++ css/Task4(02.11.2019)/script/main.js
@@ -0,0 +1,20 @@
+/*function switch_btn()
+{
+	let sw_off = document.getElementById('sw-off');
+	let sw_on = document.getElementById('sw-on');
+	if (sw_off.style.display == "block")
+	{
+		sw_off.style.display = "none";
+		sw_off.animate({
+        width: "70%",
+        opacity: 0.4,
+      }, 1500 );
+		sw_on.style.display = "block";
+	}
+	else
+	{
+		sw_off.style.display = "block";
+		sw_on.style.display = "none";
+	}
+	
+}*/
