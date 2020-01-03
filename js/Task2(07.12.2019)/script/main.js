e7e9082f3f04b5b60924b0fd9effcda68d6a6d50
MariiaRev <maria.revenko2209@gmail.com>
2020-01-03 21:33:18 +0200

    edited task2 (guessing number by user).

--- js/Task2(07.12.2019)/script/main.js
+++ js/Task2(07.12.2019)/script/main.js
@@ -1,18 +1,14 @@
 function Guess(min, max)
 {
-	// min = Math.ceil(min);
- //  	max = Math.floor(max);
   	let number = Math.floor(Math.random() * (max - min)) + min, count = 0, N;
   	console.log("number = " + number);
   	do
   	{
 	  	let str = "Вгадайте число з проміжку [" + min +";"+ max + "]";
 	  	N = max;
-	  	//console.log("до циклу ", N);
 	  	while (N >= max || N <= min)
 		{
 			N = prompt(str, min);
-			//console.log("після циклу ", N);
 			if (N==null)
 			break;
 		}
